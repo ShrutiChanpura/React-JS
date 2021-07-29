493dca1979024a52272ca79557eb6e5f81a84d89
Chanpura shruti <86774420+ShrutiChanpura@users.noreply.github.com>
2021-07-29 22:01:19 +0530

    Initial commit

---
+++ src/component/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Boot from "./Boot";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Boot />
+      <Boot />
+      <Boot />
+      <Boot />
+      <Boot />
+      <Boot />
+      <Boot />
+      <Boot />
+      <Boot />
+      <Boot />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Boot.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Boot() {
+  return (
+    <div className="note">
+      <h1> React Js Bootcamp </h1>
+      <p>
+        {" "}
+        This is the last day of the bootcamp which cover the entire project as
+        it.{" "}
+      </p>
+    </div>
+  );
+}
+
+export default Boot;
